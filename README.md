a5040061d612915d77a7c8f7bf845fdfd8e93bbf
Nguyen Thanh Nam <vietbetatester@outlook.com>
2018-07-19 23:11:44 +0800

    Better organized. Updated README.md too.

--- README.md
+++ README.md
@@ -5,31 +5,31 @@
 
 1. Git for Windows/Mac/Linux/BSD: download at https://git-scm.com/
 
-2. GitPython: install at terminal `pip3 install GitPython`
+2. GitPython: install at terminal with `pip3 install GitPython`
 
 **How to use**
 
 1. Clone this repo into your app's root directory after installing dependencies.
 
-2. In your app: `from updater import *`
+2. In your app: `import updater`
 
 3. Edit `config.py` (more details later on...)
 
-4. When you need to update, `updateAppNow()`. This will clone to the directory you defined in `config.py`. If you want to move your app data and configured `updater.py` from the old folder, have a look at *`config.py` guide* below.
+4. When you need to update, `updateAppNow()`. This will clone to the directory you defined in `config.py`. If you want to move your configured `updater.py` from the old folder, have a look at *`config.py` guide* below.
 
 **config.py guide**
 
 *A guide will also be inside the `config.py`.*
 
+*You can always use `configureConfigNow()` to configure this file.*
+
 `appName`: Name of your application.
 
-`appRepo`: The address of the Git repo, e.g https://github.com/jkelol111/updater.py.git (don't forget `.git` at the end!)
+`appRepo`: The address of the Git repo, e.g https://github.com/jkelol111/updater.py.git (don't forget `.git` at the end!).
 
-`appDir`: Your app's directory (will not change after update, update it with the new one later with `configureUpdater()`)
+`appDir`: Your app's directory.
 
-`backupOn`: Backup on/off switch. Integers other than 0 (off) or 1 (on) will be regarded as non-valid options and will not be considered. This being on will automatically copy `config.py` from the old app to the new one.
-
-`backupDir`: The directory to backup from the application's root folder.
+`backupOn`: Backup on/off switch. `True` or `False` booleen. This being on will automatically copy `config.py` from the old app to the new one.
 
 **Future plans**
 
--- updater.py
+++ updater.py
@@ -8,7 +8,6 @@ def exList(ex):
     print("- If you're getting errors about config, reread your config file.")
     print("- Make sure you have installed all the dependencies. Read README.md for more info.")
     print("If nothing works, please create a new issue on GitHub: https://github.com/jkelol111/updater.py/issues")
-    exit()
 
 try:
     from git import Repo
@@ -16,7 +15,6 @@ try:
     from os import name
     from shutil import rmtree
     from shutil import copy2
-    from shutil import copytree
     from getpass import getuser
 
     import config as cfg
@@ -25,10 +23,8 @@ try:
     appExecName = cfg.appExecName
     appDir = cfg.appDir
     backupOn = cfg.backupOn
-    backupDir = cfg.backupDir
 
     configDir = str(appDir+"/config.py")
-    extBackupDir =  str(appDir+backupDir)
     username = getuser()
     tmpBackupDirWin = str("C:/Users/"+username+"/.tmp_updater")
     tmpBackupDirNix = str("/home/"+username+"/.tmp_updater")
@@ -38,6 +34,7 @@ except Exception as e:
     exList(e)
 
 def checkConfig():
+    global configured
     if appName == "Nothing":
         configured = bool(False)
     elif appRepo == "http://www.example.com/project.git":
@@ -46,34 +43,36 @@ def checkConfig():
         configured = bool(False)
     elif appDir == "Nothing":
         configured = bool(False)
-    elif backupDir == "Nothing":
-        configured = bool(False)
     else:
         configured = bool(True)
 
-def backupNow():
-    checkConfig()
-    print("[1.5/2] Backing up your application.")
-    if configured == bool(False):
-        print("You have not configured your config.py. The updater will now terminate!")
+def restoreConfigNow():
+    try:
+        if name == "windows":
+            bckConfigDirWin = str(tmpBackupDirWin+"/config.py")
+            copy2(bckConfigDirWin, appDir)
+        elif name == "posix":
+            bckConfigDirNix = str(tmpBackupDirNix+"/config.py")
+            copy2(bckConfigDirNix, appDir)
+        else:
+            print("updater.py cannot be run on your PC. The updater will now terminate!")
+            exit()
+    except Exception as e:
+        print("There isn't anything to clean up.")
+        exList(e)
+
+def backupConfigNow():
+    print("[1.5/2] Backing up your configuration file.")
+    if name == "windows":
+        mkdir(tmpBackupDirWin)
+        copy2(configDir, tmpBackupDirWin)
+    elif name == "posix":
+        mkdir(tmpBackupDirNix)
+        copy2(configDir, tmpBackupDirNix)
+    else:
+        print("updater.py cannot be run on your PC. The updater will now terminate!")
         exit()
-    elif configured == bool(True):
-        try:
-            if name == "windows":
-                mkdir(tmpBackupDirWin)
-                copytree(appDir, tmpBackupDirWin)
-                copy2(configDir, tmpBackupDirWin)
-                copytree(appDir, tmpBackupDirWin)
-            elif name == "posix":
-                mkdir(tmpBackupDirNix)
-                copytree(appDir, tmpBackupDirNix)
-                copy2(configDir, tmpBackupDirNix)
-                copytree(backupDir, tmpBackupDirNix)
-            else:
-                print("updater.py cannot be run on your PC. The updater will now terminate!")
-                exit()
-        except Exception as e:
-            exList(e)
+    rmtree(appDir)
 
 def updateNow():
     checkConfig()
@@ -87,35 +86,16 @@ def updateNow():
             if backupOn == bool(False):
                 rmtree(appDir)
             elif backupOn == bool(True):
-                backupNow()
-                rmtree(appDir)
-                
+                backupConfigNow()
+
             print("[2/2] Downloading and installing new version of application.")
             mkdir(appDir)
             Repo.clone_from(appRepo, appDir)
 
-            print("Update/Reinstall completed.")
-        except Exception as e:
-            exList(e)
+            print("[2.5/2] Restoring app configuration files.")
+            restoreConfigNow()
 
-def revertUpdateNow():
-    if backupOn == 0:
-        print("The update cannot be reverted, since nothing is backed up.")
-    elif backupOn == 1:
-        try:
-            rmtree(appDir)
-            if name == "windows":
-                copytree(tmpBackupDirWin, tmpBackupDirWin)
-                copy2(tmpBackupDirWin+"/config.py", tmpBackupDirWin)
-                copytree(appDir, tmpBackupDirWin)
-            elif name == "posix":
-                mkdir(tmpBackupDirNix)
-                copytree(appDir, tmpBackupDirNix)
-                copy2(configDir, tmpBackupDirNix)
-                copytree(backupDir, tmpBackupDirNix)
-            else:
-                print("updater.py cannot be run on your PC. The updater will now terminate!")
-                exit()
+            print("Update/Reinstall completed.")
         except Exception as e:
             exList(e)
 
@@ -131,3 +111,6 @@ def cleanupNow():
     except Exception as e:
         print("There isn't anything to clean up.")
         exList(e)
+
+def configureConfigNow(name, repo, dir, bckOn):
+    print("[!] This function does not work yet!")
