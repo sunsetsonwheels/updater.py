5502094ec0f271d91ad03a6c073f50e5963b39a6
Nguyen Thanh Nam <vietbetatester@outlook.com>
2018-07-22 23:39:59 +0800

    Fixed more.

--- README.md
+++ README.md
@@ -9,13 +9,21 @@
 
 **How to use**
 
-1. Clone this repo into your app's root directory after installing dependencies.
+1. Download a release into your app's root directory after installing dependencies.
 
 2. In your app: `import updater`
 
 3. Edit `config.py` (more details later on...)
 
 4. When you need to update, `updateAppNow()`. This will clone to the directory you defined in `config.py`. If you want to move your configured `updater.py` from the old folder, have a look at *`config.py` guide* below.
+
+**Additional functions**
+
+1. `cleanupNow()`: Removes the temporary directory (.tmp_updater). Not recommended unless updating, where it will be executed by `updateNow()`.
+
+2. `backupConfigNow()`: Backups your `config.py` to temporary directory, even when `backupOn = bool(False)` is stated in `config.py`.
+
+3. `restoreConfigNow()`: Attempts to restore a backed up `config.py` to your application directory. Does nothing if file doesn't exist.
 
 **config.py guide**
 
@@ -30,7 +38,3 @@
 `appDir`: Your app's directory.
 
 `backupOn`: Backup on/off switch. `True` or `False` booleen. This being on will automatically copy `config.py` from the old app to the new one.
-
-**Future plans**
-
-1. Getting this to actually work.
--- updater.py
+++ updater.py
@@ -1,19 +1,59 @@
 def exList(ex):
     ex_str = str(ex)
-    from platform import python_version
     print("During the usage of updater.py, we've encountered some errors. Here's the exception:")
     print("======================================================================================")
     print(ex_str)
-    print("======================================================================================")
-    print("System information:")
-    print("======================================================================================")
-    print(System)
     print("======================================================================================")
     print("Please do the following:")
     print("- If you're getting errors about config, reread your config file.")
     print("- Make sure you have installed all the dependencies. Read README.md for more info.")
     print("If nothing works, please create a new issue on GitHub: https://github.com/jkelol111/updater.py/issues")
     exit()
+
+def logger(log, subcat):
+    try:
+        if log == "cleanup":
+            if subcat == "begin":
+                print("[i] Deleting your temporary update files.")
+            elif subcat == "done":
+                print("[i] Done deleting your temporary update files.")
+            elif subcat == "none":
+                print("[!] There is nothing to clean.")
+        elif log == "update":
+            if subcat == "begin":
+                print("[i] Update/Reinstall commence.")
+            elif subcat == "step1":
+                print("[1/3] Emptying folder of application.")
+            elif subcat == "step1.5":
+                print("[1.5/3] Backing up config.py of application.")
+            elif subcat == "step2":
+                print("[2/3] Downloading and installing new version of application.")
+            elif subcat == "step2.5":
+                print("[2.5/3] Restoring app configuration files.")
+            elif subcat == "step3":
+                print("[3/3] Downloading and installing updater.py for application.")
+            elif subcat == "done":
+                print("Update/Reinstall completed.")
+        elif log == "restore":
+            if subcat == "begin":
+                print("[i] Restoring your config.py.")
+            if subcat == "done":
+                print("[i] Done restoring your config.py.")
+            if subcat == "none":
+                print("[!] There is no config.py to restore.")
+        elif log == "backup":
+            if subcat == "begin":
+                print("[i] Backing up your config.py.")
+            if subcat == "done":
+                print("[i] Done backing up your config.py.")
+            if subcat == "none":
+                print("[!] There is no config.py to back up.")
+        elif log == "notSupported":
+            print("[!] Your OS does not support updater.py. The updater will now exit!")
+        elif log == "notConfigured":
+            print("[!] You have not configured your config.py. The updater will now terminate!")
+    except:
+        print("logger does not have that!")
 
 try:
     from git import Repo
@@ -40,11 +80,12 @@ try:
     appDir = cfg.appDir
     backupOn = cfg.backupOn
 
-    configDir = str(appDir+"/config.py")
-    updaterDir = str(appDir+"/updater.py")
+    configFile = str(appDir+"/config.py")
+    updaterFile = str(appDir+"/updater.py")
     username = getuser()
     tmpBackupDirWin = str("C:/Users/"+username+"/.tmp_updater")
     tmpBackupDirNix = str("/home/"+username+"/.tmp_updater")
+    tmpBackupDirOSX = str("/Users/"+username+"/.tmp_updater")
 
     configured = bool(False)
 except Exception as e:
@@ -66,106 +107,157 @@ def checkConfig():
     else:
         configured = bool(True)
 
-def cannotRun():
-    print("[!] updater.py cannot be run on your PC. The updater will now terminate!")
-    exit()
-
 def cleanupNow():
     try:
-        if name == "nt":
+        logger("cleanup", "begin")
+        if system() == "Windows":
             if isdir(tmpBackupDirWin) == bool(True):
                 rmtree(tmpBackupDirWin, onerror=onerrorPatch)
-        elif name == "posix":
+            elif isdir(tmpBackupDirWin) == bool(False):
+                logger("cleanup", "none")
+        elif system() == "Linux":
             if isdir(tmpBackupDirNix) == bool(True):
                 rmtree(tmpBackupDirNix)
+            elif isdir(tmpBackupDirNix) == bool(False):
+                logger("cleanup", "none")
+        elif system() == "Darwin":
+            if isdir(tmpBackupDirOSX) == bool(True):
+                rmtree(tmpBackupDirOSX)
+            elif isdir(tmpBackupDirOSX) == bool(False):
+                logger("cleanup", "none")
         else:
-            cannotRun()
+            logger("notSupported", "")
+        logger("cleanup", "done")
     except Exception as e:
-        print("There isn't anything to clean up.")
         exList(e)
 
 def restoreConfigNow():
     try:
-        if name == "nt":
-            if isdir(tmpBackupDirWin) == bool(True):
-                bckConfigDirWin = str(tmpBackupDirWin+"/config.py")
-                copy2(bckConfigDirWin, appDir)
-        elif name == "posix":
-            if isdir(tmpBackupDirNix) == bool(True):
-                bckConfigDirNix = str(tmpBackupDirNix+"/config.py")
-                copy2(bckConfigDirNix, appDir)
+        logger("restore", "begin")
+        if system() == "Windows":
+            bckconfigFileWin = str(tmpBackupDirWin+"/config.py")
+            if isfile(bckconfigFileWin) == bool(True):
+                copy2(bckconfigFileWin, appDir)
+            elif isfile(bckconfigFileWin) == bool(False):
+                logger("restore", "none")
+        elif system() == "Linux":
+            bckconfigFileNix = str(tmpBackupDirNix+"/config.py")
+            if isfile(bckconfigFileNix) == bool(True):
+                copy2(bckconfigFileNix, appDir)
+            elif isfile(bckconfigFileNix) == bool(False):
+                logger("restore", "none")
+        elif system() == "Darwin":
+            bckconfigFileOSX = str(tmpBackupDirOSX+"/config.py")
+            if isfile(bckconfigFileOSX) == bool(True):
+                copy2(bckconfigFileOSX, appDir)
+            elif isfile(bckconfigFileOSX) == bool(False):
+                logger("restore", "none")
         else:
-            cannotRun()
+            logger("notSupported", "")
+        logger("restore", "done")
     except Exception as e:
         exList(e)
 
 def backupConfigNow():
     cleanupNow()
     try:
-        if name == "nt":
+        logger("backup", "begin")
+        if system() == "Windows":
             mkdir(tmpBackupDirWin)
-            copy2(configDir, tmpBackupDirWin)
-        elif name == "posix":
+            if isfile(configFile) == bool(True):
+                copy2(configFile, tmpBackupDirWin)
+            elif isfile(configFile) == bool(False):
+                logger("backup", "none")
+        elif system() == "Linux":
             mkdir(tmpBackupDirNix)
-            copy2(configDir, tmpBackupDirNix)
+            if isfile(configFile) == bool(True):
+                copy2(configFile, tmpBackupDirNix)
+            elif isfile(configFile) == bool(False):
+                logger("backup", "none")
+        elif system() == "Darwin":
+            mkdir(tmpBackupDirWin)
+            if isfile(configFile) == bool(True):
+                copy2(configFile, tmpBackupDirWin)
+            elif isfile(configFile) == bool(False):
+                logger("backup", "none")
         else:
-            cannotRun()
+            logger("notSupported", "")
+        logger("backup", "done")
     except Exception as e:
         exList(e)
 
 def updateUpdaterNow():
     updaterRepoGit = str("https://github.com/jkelol111/updater.py.git")
-    if isfile(updaterDir) == bool(True):
-        remove(updaterDir)
-    if name == "nt":
-        updaterUpdatedDirWin = str(tmpBackupDirWin+"/updaterpy")
-        if isdir(updaterUpdatedDirWin) == bool(True):
-            rmtree(updaterUpdatedDirWin, onerror=onerrorPatch)
-        mkdir(updaterUpdatedDirWin)
-        Repo.clone_from(updaterRepoGit, updaterUpdatedDirWin)
-        updaterUpdatedFileWin = str(updaterUpdatedDirWin+"/updater.py")
-        copy2(updaterUpdatedFileWin, appDir)
-    elif name == "posix":
-        updaterUpdatedDirNix = str(tmpBackupDirNix+"/updaterpy")
-        if isdir(updaterUpdatedDirNix) == bool(True):
-            rmtree(updaterUpdatedDirNix)
-        mkdir(updaterUpdatedDirNix)
-        Repo.clone_from(updaterRepoGit, updaterUpdatedDirNix)
-        updaterUpdatedFileNix = str(updaterUpdatedDirNix+"/updater.py")
-        copy2(updaterUpdatedFileNix, appDir)
+    try:
+        logger("updateUpdater", "begin")
+        if isfile(updaterFile) == bool(True):
+            remove(updaterFile)
+        else:
+            continue
+
+        if system() == "Windows":
+            updaterUpdatedDirWin = str(tmpBackupDirWin+"/updaterpy")
+            if isdir(updaterUpdatedDirWin) == bool(True):
+                rmtree(updaterUpdatedDirWin, onerror=onerrorPatch)
+            mkdir(updaterUpdatedDirWin)
+            Repo.clone_from(updaterRepoGit, updaterUpdatedDirWin)
+            updaterUpdatedFileWin = str(updaterUpdatedDirWin+"/updater.py")
+            copy2(updaterUpdatedFileWin, appDir)
+        elif system() == "Linux":
+            updaterUpdatedDirNix = str(tmpBackupDirNix+"/updaterpy")
+            if isdir(updaterUpdatedDirNix) == bool(True):
+                rmtree(updaterUpdatedDirNix)
+            mkdir(updaterUpdatedDirNix)
+            Repo.clone_from(updaterRepoGit, updaterUpdatedDirNix)
+            updaterUpdatedFileNix = str(updaterUpdatedDirNix+"/updater.py")
+            copy2(updaterUpdatedFileNix, appDir)
+        elif system() == "Darwin":
+            updaterUpdatedDirOSX = str(tmpBackupDirOSX+"/updaterpy")
+            if isdir(updaterUpdatedDirOSX) == bool(True):
+                rmtree(updaterUpdatedDirOSX)
+            mkdir(updaterUpdatedDirOSX)
+            Repo.clone_from(updaterRepoGit, updaterUpdatedDirOSX)
+            updaterUpdatedFileOSX = str(updaterUpdatedDirOSX+"/updater.py")
+            copy2(updaterUpdatedFileOSX, appDir)
+        logger("updateUpdater", "done")
+        else:
+            logger("notSupported", "")
+    except Exception as e:
+        exList(e)
 
 def updateNow():
     checkConfig()
     if configured == bool(False):
-        print("You have not configured your config.py. The updater will now terminate!")
+        logger("notConfigured", "")
         exit()
     elif configured == bool(True):
         try:
-            print("Update/Reinstall running.")
-            print("[1/3] Emptying folder of application.")
+            logger("update", "begin")
+
+            logger("update", "step1")
             if backupOn == bool(True):
-                print("[1.5/3] Backing up config.py of application.")
+                logger("update", "step1.5")
                 backupConfigNow()
             rmtree(appDir, onerror=onerrorPatch)
 
-            print("[2/3] Downloading and installing new version of application.")
+            logger("update", "step2")
             mkdir(appDir)
             Repo.clone_from(appRepo, appDir)
 
             if backupOn == bool(True):
-                print("[2.5/3] Restoring app configuration files.")
+                logger("update", "step2.5")
                 restoreConfigNow()
 
-            print("[3/3] Downloading and installing updater.py for application.")
+            logger("update", "step3")
             updateUpdaterNow()
 
-            print("Update/Reinstall completed.")
+            logger("update", "done")
         except Exception as e:
             exList(e)
 
 def configureConfigNow(name, repo, directory, bckOn):
     try:
-        configFileWrite = open(configDir, 'w')
+        configFileWrite = open(configFile, 'w')
         configFileWrite.write("appName = '"+name+"'")
         configFileWrite.write("appRepo = '"+repo+"'")
         configFileWrite.write("appDir = '"+directory+"'")
